251b3eade684ac30eb43e5bad8cb5f57b73d3c8d
joemarney <marney.joe@gmail.com>
2025-01-21 11:45:16 +0000

    font sizes in media queries site wide

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -8,12 +8,14 @@ import { fadeScale } from "../../components/animations/animations";
 export default function About({ scrollTo }) {
   return (
     <main className={styles.container}>
-      <section id="about" className="min-h-screen flex flex-col justify-center items-center bg-theme-background2 text-theme-words px-40">
+      <section id="about" className="min-h-screen flex flex-col justify-center items-center bg-theme-background2 text-theme-words">
         <Wrapper animation={fadeScale}>
           <h1 className="text-4xl font-bold text-center m-10">about me</h1>
         </Wrapper>
         <div>
-          <p>For 8 years I worked in the Aerospace industry. There I developed my understanding of precision, problem-solving, and innovation. I love to learn and challenge myself, so I made the decision to evolve my career.</p>
+          <h2 className="mb-2">Manufacturing Engineer</h2>
+          <p>For 8 years I worked for a leading company in the Aerospace industry. I was building defense systems for the MOD and governments across the globe. There I developed my understanding of precision, problem-solving, and innovation. I love to learn and challenge myself, so I made the decision to evolve my career.</p>
+          <h2 className="mb-2 mt-2">Software Engineer</h2>
           <p>I completed the General Assembly: Software Engineering Immersive Bootcamp. This intensive 12-week course expanded my technical skill set and reinforced my passion for building solutions and solving complex issues.</p>
         </div>
         <button className="mt-10 text-5xl animate-bounce focus:outline-none" onClick={() => scrollTo("hobbies")}>
